9a1c0e93110fb2ac29501dc71c45263a7bdb39f9
Paryszek <michal.parysz@yahoo.com>
2018-05-28 16:44:05 +0200

    Added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,20 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { BlogComponent } from './main/blog/blog.component';
+import { LoginComponent } from './main/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: MainComponent
   },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   { 
-    path: 'blog-component',
+    path: 'blog',
     component: BlogComponent
   }
 ];
@@ -26,18 +32,16 @@ const appRoutes: Routes = [
     MainComponent,
     FooterComponent,
     BlogComponent,
+    LoginComponent,
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
+    FormsModule,
     BrowserModule,
     HttpClientModule
   ],
   providers: [],
 
-  bootstrap: [
-    AppComponent,
-    NavigationComponent,
-    FooterComponent
-  ]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
