370fe8ecad997b993564c771d034181f3e48221e
Paryszek <michal.parysz@yahoo.com>
2018-06-05 19:43:58 +0200

    User Model

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 class User {
-  id: number;
   login: string;
   password: string;
   constructor(login: string, password: string) {
@@ -17,8 +16,7 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   public loginUser(login: string, password: string) {
-    let user: User = new User(login, password);
-    user.id = 1;
+    const user: User = new User(login, password);
     return this.http.post('http://localhost:8080/login', user).subscribe(
       res => {
         // przeniesienie do bloga jesli sukces
